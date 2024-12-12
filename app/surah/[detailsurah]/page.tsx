85b7539b9fb8285d207c53f51be3e7bc494a9383
Tri-Hartono <Trihartono293@gmail.com>
2024-12-12 15:30:04 +0700

    add audio player

--- app/surah/[detailsurah]/page.tsx
+++ app/surah/[detailsurah]/page.tsx
@@ -1,100 +1,119 @@
-"use client";
-import React, { Suspense, useState } from "react";
-import SuratNavigation from "@/components/SuratNavigation";
-import { Ayat } from "@/types/Ayat";
-import { DataSurah } from "@/types/Surah";
-import axios from "axios";
-import { HiSearch } from "react-icons/hi";
-import Link from "next/link";
-import Loading from "../loading";
+'use client';
+import React, { Suspense, useState } from 'react';
+import SuratNavigation from '@/components/SuratNavigation';
+import { Ayat } from '@/types/Ayat';
+import { DataSurah } from '@/types/Surah';
+import axios from 'axios';
+import { HiSearch } from 'react-icons/hi';
+import Link from 'next/link';
+import Loading from '../loading';
 
 const getDataUsers = async (param: string) => {
-   const res = await axios.get(`https://equran.id/api/v2/surat/${param}`);
-   return res.data;
+    const res = await axios.get(`https://equran.id/api/v2/surat/${param}`);
+    return res.data;
 };
 
 export default async function DetailSurah({
-   params,
+    params,
 }: {
-   params: { detailsurah: string };
+    params: { detailsurah: string };
 }) {
-   const [modal, setModal] = useState(false);
-   const dataSurah = await getDataUsers(params.detailsurah);
+    const [modal, setModal] = useState(false);
+    const dataSurah = await getDataUsers(params.detailsurah);
 
-   const suratSelanjutnya: DataSurah = dataSurah.data.suratSelanjutnya;
-   const suratSebelumnya: DataSurah = dataSurah.data.suratSebelumnya;
+    const suratSelanjutnya: DataSurah = dataSurah.data.suratSelanjutnya;
+    const suratSebelumnya: DataSurah = dataSurah.data.suratSebelumnya;
 
-   const isSuratSelesai = !suratSelanjutnya;
-   return (
-      <div className="py-10 relative">
-         <Suspense
-            fallback={
-               <div>
-                  <Loading />
-               </div>
-            }
-         >
-            {/*@ts-ignore*/}
-            <div className="text-center text-3xl">
-               Surah : {dataSurah.data.namaLatin}
-            </div>
-            <div className="space-y-4 py-10">
-               {dataSurah.data.ayat.map((item: Ayat, index: number) => (
-                  <div
-                     key={index}
-                     className="space-y-6 border-t pt-6  border-blue-500  relative"
-                  >
-                     <h2 className="absolute top-0 left-0 py-1 px-2 bg-blue-500  text-white rounded-br-xl">
-                        {item.nomorAyat}
-                     </h2>
-                     <h2 className="text-right leading-loose mb-2 py-4 text-2xl font-semibold">
-                        {item.teksArab}
-                     </h2>
-                     <div className="space-y-2">
-                        <p className="font-semibold">{item.teksLatin}</p>
-                        <p>{item.teksIndonesia}</p>
-                     </div>
-                  </div>
-               ))}
-            </div>
-            <div className="flex gap-2  flex-col md:flex-row">
-               {suratSebelumnya && (
-                  <SuratNavigation
-                     surat={suratSebelumnya}
-                     label="Surat Sebelumnya"
-                  />
-               )}
-               {isSuratSelesai ? (
-                  <h2>Ini adalah surat terakhir</h2>
-               ) : (
-                  <SuratNavigation
-                     surat={suratSelanjutnya}
-                     label="Surat Selanjutnya"
-                  />
-               )}
-            </div>
-         </Suspense>
+    const isSuratSelesai = !suratSelanjutnya;
 
-         <div
-            onClick={() => setModal(true)}
-            className="p-2 bg-blue-400 text-zinc-100 rounded-md fixed z-20 right-4 bottom-10"
-         >
-            <HiSearch className="w-5 h-5" />
-         </div>
-         {modal && (
-            <div className="fixed z-10 bg-white/90 flex flex-col text-center dark:bg-zinc-900/90 dark:text-white items-center justify-center inset-0 space-y-10">
-               <h1>Apa anda ingin melakukan pencarian ?</h1>
-               <div className="flex gap-4 items-center">
-                  <Link
-                     href="/surah"
-                     className="py-2 px-4 bg-blue-400 text-white rounded-md"
-                  >
-                     Ya
-                  </Link>
-                  <div onClick={() => setModal(false)}>No</div>
-               </div>
+    return (
+        <div className='py-10 relative'>
+            <Suspense
+                fallback={
+                    <div>
+                        <Loading />
+                    </div>
+                }
+            >
+                <div className='text-center text-3xl'>
+                    Surah : {dataSurah.data.namaLatin}
+                </div>
+                <div className='space-y-4 py-10'>
+                    {dataSurah.data.ayat.map((item: Ayat, index: number) => (
+                        <div
+                            key={index}
+                            className='space-y-6 border-t pt-6 border-blue-500 relative'
+                        >
+                            <h2 className='absolute top-0 left-0 py-1 px-2 bg-blue-500 text-white rounded-br-xl'>
+                                {item.nomorAyat}
+                            </h2>
+                            <h2 className='text-right leading-loose mb-2 py-4 text-2xl font-semibold'>
+                                {item.teksArab}
+                            </h2>
+                            <div className='space-y-2'>
+                                <p className='font-semibold'>
+                                    {item.teksLatin}
+                                </p>
+                                <p>{item.teksIndonesia}</p>
+                                {item.audio &&
+                                    Object.keys(item.audio).length > 0 && (
+                                        <div className='w-full'>
+                                            <audio controls className='w-full'>
+                                                <source
+                                                    src={
+                                                        item.audio[
+                                                            Object.keys(
+                                                                item.audio
+                                                            )[0]
+                                                        ]
+                                                    }
+                                                    type='audio/mpeg'
+                                                />
+                                            </audio>
+                                        </div>
+                                    )}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div className='flex gap-2 flex-col md:flex-row'>
+                    {suratSebelumnya && (
+                        <SuratNavigation
+                            surat={suratSebelumnya}
+                            label='Surat Sebelumnya'
+                        />
+                    )}
+                    {isSuratSelesai ? (
+                        <h2>Ini adalah surat terakhir</h2>
+                    ) : (
+                        <SuratNavigation
+                            surat={suratSelanjutnya}
+                            label='Surat Selanjutnya'
+                        />
+                    )}
+                </div>
+            </Suspense>
+
+            <div
+                onClick={() => setModal(true)}
+                className='p-2 bg-blue-400 text-zinc-100 rounded-md fixed z-20 right-4 bottom-10'
+            >
+                <HiSearch className='w-5 h-5' />
             </div>
-         )}
-      </div>
-   );
+            {modal && (
+                <div className='fixed z-10 bg-white/90 flex flex-col text-center dark:bg-zinc-900/90 dark:text-white items-center justify-center inset-0 space-y-10'>
+                    <h1>Apa anda ingin melakukan pencarian ?</h1>
+                    <div className='flex gap-4 items-center'>
+                        <Link
+                            href='/surah'
+                            className='py-2 px-4 bg-blue-400 text-white rounded-md'
+                        >
+                            Ya
+                        </Link>
+                        <div onClick={() => setModal(false)}>No</div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 }
--- store/content-slice.ts
+++ store/content-slice.ts
@@ -3,57 +3,61 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { ErrorFetchDataResponse } from '@/types/type';
 import axios, { AxiosError } from 'axios';
 
-export const fetchSurat = createAsyncThunk<SurahType, null, { rejectValue: ErrorFetchDataResponse }>('data/fetch-surah', async (_, { rejectWithValue }) => {
-  try {
-    const res = await axios.get('https://equran.id/api/v2/surat');
-    return res.data;
-  } catch (err) {
-    const error = err as AxiosError<ErrorFetchDataResponse>;
-    if (!error.response) {
-      throw error;
+export const fetchSurat = createAsyncThunk<
+    SurahType,
+    null,
+    { rejectValue: ErrorFetchDataResponse }
+>('data/fetch-surah', async (_, { rejectWithValue }) => {
+    try {
+        const res = await axios.get('https://equran.id/api/v2/surat');
+        return res.data;
+    } catch (err) {
+        const error = err as AxiosError<ErrorFetchDataResponse>;
+        if (!error.response) {
+            throw error;
+        }
+        return rejectWithValue(error.response.data);
     }
-    return rejectWithValue(error.response.data);
-  }
 });
 
 interface State {
-  surat: SurahType | undefined;
-  currentContentType: string;
-  status: string;
-  error: undefined | object;
+    surat: SurahType | undefined;
+    currentContentType: string;
+    status: string;
+    error: undefined | object;
 }
 
 const initialState: State = {
-  surat: undefined,
-  currentContentType: 'surat',
-  status: 'idle',
-  error: undefined,
+    surat: undefined,
+    currentContentType: 'surat',
+    status: 'idle',
+    error: undefined,
 };
 
 const contentSlice = createSlice({
-  name: 'content',
-  initialState,
-  reducers: {
-    setCurrentType(state, action) {
-      state.currentContentType = action.payload;
+    name: 'content',
+    initialState,
+    reducers: {
+        setCurrentType(state, action) {
+            state.currentContentType = action.payload;
+        },
     },
-  },
-  extraReducers(builder) {
-    builder
-      .addCase(fetchSurat.pending, (state) => {
-        state.status = 'loading';
-        state.surat = undefined;
-        state.error = undefined;
-      })
-      .addCase(fetchSurat.fulfilled, (state, action) => {
-        state.surat = action.payload;
-        state.status = 'succeeded';
-      })
-      .addCase(fetchSurat.rejected, (state, action) => {
-        state.status = 'error';
-        state.error = action.payload;
-      });
-  },
+    extraReducers(builder) {
+        builder
+            .addCase(fetchSurat.pending, (state) => {
+                state.status = 'loading';
+                state.surat = undefined;
+                state.error = undefined;
+            })
+            .addCase(fetchSurat.fulfilled, (state, action) => {
+                state.surat = action.payload;
+                state.status = 'succeeded';
+            })
+            .addCase(fetchSurat.rejected, (state, action) => {
+                state.status = 'error';
+                state.error = action.payload;
+            });
+    },
 });
 
 export const { setCurrentType } = contentSlice.actions;
